8bda8d833f9884aa057542acd3e463b9362852c8
Isaac Navarro <isaacnavarro2000@gmail.com>
2025-03-05 12:45:36 -0500

    fix: se cambió a una b minuscula para evitar errores en los interceptores

--- src/service/axiosConfig.ts
+++ src/service/axiosConfig.ts
@@ -10,7 +10,7 @@ axiosInstance.interceptors.request.use(
     const token = localStorage.getItem("access_token");
 
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers["Authorization"] = `bearer ${token}`;
     }
 
     return config;
